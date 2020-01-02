671584e759b1d36544ba339e142ebdd0b8045e5b
IrfanMustafa <irfanmustafa31201@gmail.com>
2020-01-02 02:12:19 -0500

    changes dice rol

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+
+
+var scores, roundscores, activeplayer, dice1, dice2, gamePlaying, previousscore;
+init();
+
+//document.querySelector('#current-' + activeplayer).textContent = dice;
+//document.querySelector('#current-' + activeplayer).innerHTML = '<em>' + dice; '</em>';
+
+//var x = document.querySelector('#score-' + activeplayer).textContent;
+//console.log(x);
+
+//document.querySelector('.dice').style.display = 'none';
+
+//function btn() {
+
+//}
+//btn();
+
 
 document.querySelector('.btn-new').addEventListener('click', init);
 
@@ -11,7 +39,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         document.getElementById('dice-2').style.display = 'block';
         document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
         document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
-      
+        //console.log(dice1, dice2);
 
 
         if (dice1 !== 1 && dice2 !== 1) {
@@ -56,7 +84,9 @@ function nextPlayer() {
 
     document.querySelector('.player-0-panel').classList.toggle('active');
     document.querySelector('.player-1-panel').classList.toggle('active');
-;
+
+    //document.querySelector('.player-0-panel').classList.remove('active');
+    //document.querySelector('.player-1-panel').classList.add('active');
 
     hidedice();
 }
